b2534e58cf74e3f65a9114b2640a8318c01dc072
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-03-16 17:39:45 -0300

    logout automatico

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
+import { signInOut } from "../contexts/AuthContext";
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -74,7 +75,10 @@ api.interceptors.response.use(
           });
         });
       } else {
+        signInOut();
       }
     }
+
+    return Promise.reject(error);
   }
 );
